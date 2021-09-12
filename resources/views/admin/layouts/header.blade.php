1449a50aaf101b19d0bf0e9eca94e6099ed9a920
hun756 <ekemenms@hotmail.com>
2021-09-12 21:37:12 +0300

    Fix Invalid Link

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -8,7 +8,7 @@
         <meta name="keywords" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
-        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+        <link rel="icon" href={{asset('template/favicon.ico')}} type="image/x-icon" />
 
         <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800" rel="stylesheet">
         
@@ -25,7 +25,7 @@
         <link rel="stylesheet" href={{asset('template/plugins/owl.carousel/dist/assets/owl.carousel.min.css')}}>
         <link rel="stylesheet" href={{asset('template/plugins/owl.carousel/dist/assets/owl.theme.default.min.css')}}>
         <link rel="stylesheet" href={{asset('template/dist/css/theme.min.css')}}>
-        <script src="src/js/vendor/modernizr-2.8.3.min.js"></script>
+        <script src={{asset('template/src/js/vendor/modernizr-2.8.3.min.js')}}></script>
     </head>
 
     <body>
