5139b2777842e561051a8296473d576c5089e84a
hun756 <ekemenms@hotmail.com>
2021-09-12 20:20:16 +0300

    Expand Form and Add Gender Attribute

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,11 +61,28 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <label for="gender_conf" class="col-md-4 col-form-label text-md-right">{{ __('Gender') }}</label>
+
+                            <div class="col-md-6">
+                                <select id="gender_conf" name="gender" class="form-control @error('password') is-invalid @enderror">
+                                    <option value="">Please Select Gender</option>
+                                    <option value="Male">Male</option>
+                                    <option value="Female">Female</option>
+                                </select>
+                            </div>
+                            @error('gender')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Register') }}
-                                </button>
+                                </button>   
                             </div>
                         </div>
                     </form>
